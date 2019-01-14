2d312be3114a3cf398a29c10c3629c1f045a915e
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-14 22:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, obj, {[key]: value});
+  delete newObject.key;
+
+  return newObject;
+  // delete object.key;
+  // returns object;
+}
